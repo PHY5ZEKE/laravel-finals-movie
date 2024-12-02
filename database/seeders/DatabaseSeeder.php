fe0334457e04f7a46b0e31d2b1d58593cfa88ab5
Ezequiel Gonzalez <ezequiel.gonzalez.cics@ust.edu.ph>
2024-12-03 07:38:00 +0800

    seeder movie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,36 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Movie;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Movie::create([
+            'title' => 'Inception',
+            'genre' => 'Sci-Fi',
+            'duration' => 148,
+            'releaseDate' => '2010-07-16',
+            'description' => 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.'
         ]);
+
+        Movie::create([
+            'title' => 'The Dark Knight',
+            'genre' => 'Action',
+            'duration' => 152,
+            'releaseDate' => '2008-07-18',
+            'description' => 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.'
+        ]);
+
+        Movie::create([
+            'title' => 'Interstellar',
+            'genre' => 'Adventure',
+            'duration' => 169,
+            'releaseDate' => '2014-11-07',
+            'description' => 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.'
+        ]);
+
     }
 }
